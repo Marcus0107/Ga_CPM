23e7ca22c63c7218f1d3be4cbea4777100da6097
Marcus0107 <danzer.marcus@gmail.com>
2017-10-25 18:32:57 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -10,10 +10,25 @@ namespace Genetischer_Algorithmus
     {
         static void Main(string[] args)
         {
-           // run_GA_Algorith(5, 50, 5, 50);
+            int cityCount = 6;
+            int roundCount = 5000;
+            int mutationProbability = 5;
+            int populationSize = 50;
 
-           // run_brute_force(5);
+            long startTimeInMilliseconds = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
 
+            run_GA_Algorith(cityCount, roundCount, mutationProbability, populationSize);
+
+            long endTimeInMilliseconds = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+            Debug.WriteLine("Genetic Algorithmn \t City count: " + cityCount + "\tRuntime: " + (endTimeInMilliseconds - startTimeInMilliseconds) + " ms");
+
+
+            startTimeInMilliseconds = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+
+            run_brute_force(cityCount);
+
+            endTimeInMilliseconds = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
+            Debug.WriteLine("Brute Force \t City count: " + cityCount + "\tRuntime: " + (endTimeInMilliseconds - startTimeInMilliseconds) + " ms");
         }
 
         public static void run_brute_force(int cityCount)
@@ -28,15 +43,13 @@ namespace Genetischer_Algorithmus
             Population pop = new Population(indis);
             pop.calculateFittnessForPopulation();
             pop.sortByFitnessDescending();
-
-            Debug.WriteLine("Fitness:\t" + pop.population[0].fittnes + "\tSequenz" + pop.population[0].ToString());
         }
 
         public static void run_GA_Algorith(int cityCount, int roundCount, int mutationProbabilityInPercent, int populationSize = 50)
         {
             //Erstes Gen in reihenfolge aus der Liste
             //@ vor dem Pfad ist wichtig
-            List<Allel> allIndividumGen = readCitiesFromFile(@"C:\Users\Marcus\Dropbox\Master Semester 2\Genetische Algorithmen\76_städte.csv");
+            List<Allel> allIndividumGen = readCitiesFromFile(@"C:\Users\Marcus\Dropbox\Master Semester 2\Genetische Algorithmen\280_städte.csv");
             List<Allel> individumGen = allIndividumGen.GetRange(0, cityCount);
 
             Population pop = new Population();
@@ -77,10 +90,10 @@ namespace Genetischer_Algorithmus
                 ga.parents.sortByFitnessDescending();
 
                 //Schreibe den besten aus der Population auf die Konsole
-                Debug.WriteLine(ga.parents.population[0].fittnes);
+
                 if (i % 500 == 0)
                 {
-                    Console.WriteLine(i + "\t" + ga.parents.population[0].fittnes);
+                    //  Debug.WriteLine(i + "\t\t Fitness of the best from the population: " + ga.parents.population[0].fittnes);
                     GC.Collect();
                 }
                 ga.children = new Population();
